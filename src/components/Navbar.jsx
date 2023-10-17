a458ad7e3d617286b56dbb6539d4567d802f0a89
Yoga Pratama <yogapr.dev@gmail.com>
2023-10-17 15:55:33 +0700

    add computer component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
           >
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
             <p className="text-white text-[18px] font-bold cursor-pointer flex">
-              Web &nbsp; <span className="sm:block hidden">| JS Mastery </span>
+              Web &nbsp; <span className="sm:block hidden">Portfolio </span>
             </p>
           </Link>
           <ul className="list-none hidden sm:flex flex-row gap-10">
