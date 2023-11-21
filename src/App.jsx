145235dab11d9e2df53101e81f1250d90853f633
Yoga Pratama <yogapr.dev@gmail.com>
2023-11-21 20:56:14 +0700

    fix: typo

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>A
+    <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
